17b6ac53601a6531f13e252e43782ceb30f33a50
Hernan Saldarriaga <hsaldarriaga@uninorte.edu.co>
2015-03-14 10:10:04 -0500

    Commit Hernan1

---
+++ app/src/main/java/com/pixelcode/appcamp/wordglish/ThreadCreator.java
@@ -0,0 +1,29 @@
+package com.pixelcode.appcamp.wordglish;
+
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by hsaldarriaga on 14/03/2015.
+ */
+public class ThreadCreator implements Runnable{
+
+    private Context c;
+
+    public ThreadCreator(Context c) {
+        this.c = c;
+    }
+
+    @Override
+    public void run() {
+        //Invocar Base de Datos
+
+
+    }
+
+
+    public void ExecuteNextActivity()
+    {
+        Intent intent = new Intent()
+    }
+}
