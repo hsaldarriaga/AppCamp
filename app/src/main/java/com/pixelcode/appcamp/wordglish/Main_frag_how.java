6baa390925cdf889a597ad69481a45d024ae8957
Hernan Saldarriaga <hsaldarriaga@uninorte.edu.co>
2015-03-14 12:08:45 -0500

    Commit Hernan2

---
+++ app/src/main/java/com/pixelcode/appcamp/wordglish/Main_frag_how.java
@@ -0,0 +1,20 @@
+package com.pixelcode.appcamp.wordglish;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by hsaldarriaga on 14/03/2015.
+ */
+public class Main_frag_how extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_how, container, false);
+    }
+}
