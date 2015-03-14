894d735255d2af54361b1832c5d6d07cb8b2d651
Hernan Saldarriaga <hsaldarriaga@uninorte.edu.co>
2015-03-14 15:10:03 -0500

    Commit Hernan7

--- app/src/main/java/com/pixelcode/appcamp/wordglish/Database/SingletonDB.java
+++ app/src/main/java/com/pixelcode/appcamp/wordglish/Database/SingletonDB.java
@@ -33,26 +33,43 @@ public class SingletonDB {
 
         //Playa
         ContentValues values = new ContentValues(); values.put(Database.Palid,"1");values.put(Database.Palpalabra,"Umbrella");db.insert(Database.Tabla_Palabra,null,values);
-        values = new ContentValues(); values.put(Database.Palid,"2");values.put(Database.Palpalabra,"Fish");db.insert(Database.Tabla_Palabra,null,values);
-        values = new ContentValues(); values.put(Database.Palid,"3");values.put(Database.Palpalabra,"Vessel");db.insert(Database.Tabla_Palabra,null,values);
-        values = new ContentValues(); values.put(Database.Palid,"4");values.put(Database.Palpalabra,"Grit");db.insert(Database.Tabla_Palabra,null,values);
-        values = new ContentValues(); values.put(Database.Palid,"5");values.put(Database.Palpalabra,"SalesClerk");db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 2);values.put(Database.Palpalabra,"Fish");db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 3);values.put(Database.Palpalabra,"Vessel");db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 4);values.put(Database.Palpalabra,"Grit");db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 5);values.put(Database.Palpalabra,"SalesClerk");db.insert(Database.Tabla_Palabra,null,values);
 
         //Desierto
-        values = new ContentValues(); values.put(Database.Palid,"6");values.put(Database.Palpalabra,"Serpent");db.insert(Database.Tabla_Palabra,null,values);
-        values = new ContentValues(); values.put(Database.Palid,"7");values.put(Database.Palpalabra,"Mirage");db.insert(Database.Tabla_Palabra,null,values);
-        values = new ContentValues(); values.put(Database.Palid,"8");values.put(Database.Palpalabra,"Palm");db.insert(Database.Tabla_Palabra,null,values);
-        values = new ContentValues(); values.put(Database.Palid,"9");values.put(Database.Palpalabra,"Camel");db.insert(Database.Tabla_Palabra,null,values);
-        values = new ContentValues(); values.put(Database.Palid,"10");values.put(Database.Palpalabra,"Overcast");db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 6);values.put(Database.Palpalabra,"Serpent");db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 7);values.put(Database.Palpalabra,"Mirage");db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 8);values.put(Database.Palpalabra,"Palm");db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 9);values.put(Database.Palpalabra,"Camel");db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 10);values.put(Database.Palpalabra,"Overcast");db.insert(Database.Tabla_Palabra,null,values);
 
         //Montaña
-        values = new ContentValues(); values.put(Database.Palid,"11");values.put(Database.Palpalabra,"Overcoat");db.insert(Database.Tabla_Palabra,null,values);
-        values = new ContentValues(); values.put(Database.Palid,"12");values.put(Database.Palpalabra,"Peak");db.insert(Database.Tabla_Palabra,null,values);
-        values = new ContentValues(); values.put(Database.Palid,"13");values.put(Database.Palpalabra,"Cable Railway");db.insert(Database.Tabla_Palabra,null,values);
-        values = new ContentValues(); values.put(Database.Palid,"14");values.put(Database.Palpalabra,"Landsalide");db.insert(Database.Tabla_Palabra,null,values);
-        values = new ContentValues(); values.put(Database.Palid,"15");values.put(Database.Palpalabra,"Skier");db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 11);values.put(Database.Palpalabra,"Overcoat");db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 12);values.put(Database.Palpalabra,"Peak");db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 13);values.put(Database.Palpalabra,"Cable Railway");db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 14);values.put(Database.Palpalabra,"Landslide");db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 15);values.put(Database.Palpalabra,"Skier");db.insert(Database.Tabla_Palabra,null,values);
 
-
+        //Bosques
+        values = new ContentValues(); values.put(Database.Palid, 0); values.put(Database.Palpalabra,"Leafs"); db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 0); values.put(Database.Palpalabra,"Tent"); db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 0); values.put(Database.Palpalabra,"Igniter"); db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 0); values.put(Database.Palpalabra,"Bruin"); db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 0); values.put(Database.Palpalabra,"Firewood"); db.insert(Database.Tabla_Palabra,null,values);
+        //Ciudad
+        values = new ContentValues(); values.put(Database.Palid, 0); values.put(Database.Palpalabra,"Skyscraper"); db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 0); values.put(Database.Palpalabra,"Eatery"); db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 0); values.put(Database.Palpalabra,"Highway"); db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 0); values.put(Database.Palpalabra,"Sewer"); db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 0); values.put(Database.Palpalabra,"Fuss"); db.insert(Database.Tabla_Palabra,null,values);
+        //Parque de Diversiones
+        values = new ContentValues(); values.put(Database.Palid, 0); values.put(Database.Palpalabra,"Scenic Railway"); db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 0); values.put(Database.Palpalabra,"Beverages"); db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 0); values.put(Database.Palpalabra,"Candyfloss"); db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 0); values.put(Database.Palpalabra,"Target Shooting"); db.insert(Database.Tabla_Palabra,null,values);
+        values = new ContentValues(); values.put(Database.Palid, 0); values.put(Database.Palpalabra,"Toffee"); db.insert(Database.Tabla_Palabra,null,values);
 
         //values.put();
 
